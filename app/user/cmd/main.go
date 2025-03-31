4cca4f839fb98f6945eed204cd4d708335c91007
nanstis <bleyo@alphomega.org>
2025-04-01 00:21:20 +0200

    feat: seperate commands for app handler

--- app/user/cmd/main.go
+++ app/user/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"embed"
+	"fmt"
 	"github.com/alpha-omega-corp/cloud/app/user/pkg"
 	"github.com/alpha-omega-corp/cloud/app/user/pkg/models"
 	"github.com/alpha-omega-corp/cloud/app/user/pkg/proto"
@@ -17,6 +18,7 @@ import (
 	"github.com/uptrace/bun/migrate"
 	"github.com/urfave/cli/v2"
 	"google.golang.org/grpc"
+	"log"
 	"os"
 )
 
@@ -29,12 +31,13 @@ type App struct {
 	name      string
 	config    types.Config
 	dbHandler *database.Handler
-	models    interface{}
+	dbModels  interface{}
 }
 
 func NewApp(efs embed.FS, name string) *App {
 	configFile, err := efs.ReadFile(config.GetConfigPath())
 	if err != nil {
+		fmt.Printf("read config file error: %v\n", err)
 		panic(err)
 	}
 
@@ -48,66 +51,76 @@ func NewApp(efs embed.FS, name string) *App {
 }
 
 func (app *App) WithModels(models ...interface{}) *App {
-	app.dbHandler.Database().RegisterModel(models)
-	app.models = models
 
 	return app
 }
 
 func (app *App) Start(init func(config types.Config, db *bun.DB, grpc *grpc.Server)) {
-	if err := srv.NewGRPC(app.config.Url, app.dbHandler, func(db *bun.DB, grpc *grpc.Server) {
-		init(app.config, db, grpc)
-	}); err != nil {
-		panic(err)
-	}
-
 	appCli := &cli.App{
 		Name:  app.name,
 		Usage: "cloud application cli",
 		Commands: []*cli.Command{
-			app.startCommand(),
+			app.startCommand(init),
 			app.migrateCommand(),
 		},
 	}
 
 	if err := appCli.Run(os.Args); err != nil {
+		fmt.Printf("app start error: %v\n", err)
 		panic(err)
 	}
 }
 
-func (app *App) startCommand() *cli.Command {
+func (app *App) bootstrap() {
+
+}
+
+func (app *App) startCommand(init func(config types.Config, db *bun.DB, grpc *grpc.Server)) *cli.Command {
 	return &cli.Command{
 		Name: "server",
 		Action: func(c *cli.Context) error {
+			if err := srv.NewGRPC(app.config.Url, app.dbHandler, func(db *bun.DB, grpc *grpc.Server) {
+				init(app.config, db, grpc)
+			}); err != nil {
+				panic(err)
+			}
+
 			return nil
 		},
 	}
 }
 
 func (app *App) migrateCommand() *cli.Command {
+	// TODO : pass models (err = passing struct to interface)
+	app.dbModels = ((*models.Service)(nil))
+
+	if app.dbModels == nil {
+		log.Fatalf("db models is nil, crashing...")
+	}
+
 	db := app.dbHandler.Database()
-	db.RegisterModel(app.models)
+	db.RegisterModel(app.dbModels)
 
 	return &cli.Command{
 		Name: "db",
 		Subcommands: []*cli.Command{
 			{
-				Name:  "init",
-				Usage: "create migration tables",
+				Name:  "migration",
+				Usage: "run migration",
 				Action: func(c *cli.Context) error {
 					migrator := migrate.NewMigrator(db, migrate.NewMigrations())
-					return migrator.Init(c.Context)
-				},
-			},
-			{
-				Name: "reset",
-				Action: func(c *cli.Context) error {
-					if err := db.ResetModel(c.Context, app.models); err != nil {
+
+					if err := migrator.Init(c.Context); err != nil {
+						return err
+					}
+
+					if err := db.ResetModel(c.Context, app.dbModels); err != nil {
 						return err
 					}
 
 					fixture := dbfixture.New(db)
-					if err := fixture.Load(c.Context, os.DirFS("cmd/migrations/fixtures"), "fixture.yml"); err != nil {
+					if err := fixture.Load(c.Context, os.DirFS("cmd/fixtures"), "fixture.yml"); err != nil {
+						fmt.Printf("load fixture error: %v\n", err)
 						panic(err)
 					}
 
@@ -119,9 +132,16 @@ func (app *App) migrateCommand() *cli.Command {
 }
 
 func main() {
-	NewApp(embedFS, "user").WithModels(
-		(*models.UserToRole)(nil),
-	).Start(func(config types.Config, db *bun.DB, grpc *grpc.Server) {
+	/*
+		WithModels(
+					(*models.User)(nil),
+					(*models.Role)(nil),
+					(*models.UserToRole)(nil),
+					(*models.Service)(nil),
+					(*models.Permission)(nil),
+				).
+	*/
+	NewApp(embedFS, "user").Start(func(config types.Config, db *bun.DB, grpc *grpc.Server) {
 		auth := utils.NewAuthWrapper(config.Env.GetString("secret"))
 		proto.RegisterUserServiceServer(grpc, pkg.NewServer(db, auth))
 	})
